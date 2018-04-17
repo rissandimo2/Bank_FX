5f6637d932e80cf241fee345303b52708b361003
= <nassiromid@gmail.com>
2018-04-17 14:17:30 -0700

    added constructor for MVC

--- src/controller/WelcomeScreenController.java
+++ src/controller/WelcomeScreenController.java
@@ -2,15 +2,23 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import model.BankConnection;
 import view.WelcomeScreen;
 
 public class WelcomeScreenController implements EventHandler<ActionEvent>
 {
+    private BankConnection bankConnection;
     private WelcomeScreen view;
 
     public WelcomeScreenController(WelcomeScreen view)
     {
         this.view = view;
+    }
+
+    public WelcomeScreenController(BankConnection bankConnection, WelcomeScreen view)
+    {
+        this.bankConnection = new BankConnection();
+        this.view = new WelcomeScreen();
     }
 
     public void handle(ActionEvent event)
